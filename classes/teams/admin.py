a5f132f3cd1e9223fd533f138378c76240c408be
Marcin Fudala <nerko937@mail.com>
2018-11-25 11:57:33 +0100

    Update admin panel

--- classes/teams/admin.py
+++ classes/teams/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CourseType, Group, Student
 
-# Register your models here.
+
+@admin.register(CourseType)
+class CourseTypeAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+	pass
